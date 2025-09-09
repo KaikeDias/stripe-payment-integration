1caa5fd7b2943dea5672b13647b4c213a6dffff7
Kaike <kaikedias98@gmail.com>
2025-09-09 14:31:55 -0300

    initial commit

---
+++ src/main/java/com/dias/payment_stripe/PaymentStripeApplication.java
@@ -0,0 +1,13 @@
+package com.dias.payment_stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentStripeApplication.class, args);
+	}
+
+}
